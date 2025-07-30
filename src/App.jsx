bb1e553452bd88e523e984e51148962f33048c1c
Polok Banik <polokbanik7@gmail.com>
2025-07-31 01:53:42 +0600

    add title and about section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import Programs from "./components/programs/Programs";
+import Title from "./components/title/Title";
+import About from "./components/about/About";
 
 export default function App() {
   return (
@@ -9,7 +11,10 @@ export default function App() {
       <Navbar />
       <Hero />
       <div className="container">
+        <Title subTitle="Our PROGRAM" title="What We Offer" />
         <Programs />
+        <About />
+        <Title subTitle="Gallery" title="Campus Photos" />
       </div>
     </div>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./navbar.css";
 import logo from "../../assets/logo.png";
 
 export default function Navbar() {
+  const [sticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setSticky(true) : setSticky(false);
+    });
+  }, []);
+
   return (
-    <nav className="container">
-      <img src={logo} alt="logo image" className="logo" />
+    <nav className={`container ${sticky ? "dark-nav" : ""}`}>
+      <img src={logo} alt="logo image" className="logo " />
 
       <ul>
         <li>Home</li>
