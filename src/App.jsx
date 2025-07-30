4cee600e746ef3af92e7c85f3024b096e5436a3d
Polok Banik <polokbanik7@gmail.com>
2025-07-31 01:10:29 +0600

    add program section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import Programs from "./components/programs/Programs";
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Programs />
+      </div>
     </div>
   );
 }
